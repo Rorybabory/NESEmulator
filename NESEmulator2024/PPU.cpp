a28625e7cb666f612e2d18f0b4d2da641feaf480
Rorybabory <lambertrory123@gmail.com>
2024-11-28 20:42:26 -0800

    Began PPU tile drawing

--- NESEmulator2024/PPU.cpp
+++ NESEmulator2024/PPU.cpp
@@ -2,33 +2,63 @@
 #include "Bus.h"
 #include "imgui/imgui.h"
 #include <glad/glad.h>
+#include "palette.h"
+
+
+
 uint8_t PPU::ReadRegister(uint8_t index) {
 	return registers[index];
 }
 void PPU::WriteRegister(uint8_t index, uint8_t bits) {
 	registers[index] = bits;
 }
+void PPU::ClearScreen() {
+	for (int x = 0; x < 256; ++x) {
+		for (int y = 0; y < 224; ++y) {
+			int pos = (y * 256 + x);
+			screen_data[pos] = x % 64;
+		}
+	}
+}
+void PPU::DrawTile(unsigned int index, uint8_t x, uint8_t y, uint8_t colors[4]) {
+	index += 0x8000;
+	
+	for (unsigned int scr_y = 0; scr_y < 8; ++scr_y) {
+		uint8_t layer_1 = bus->Read(index + scr_y);
+		uint8_t layer_2 = bus->Read(index + scr_y + 8);
+		for (unsigned int scr_x = 0; scr_x < 8; ++scr_x) {
+			int color = 0;
+			if (layer_1 && (1 << scr_x) == (1 << scr_x)) {
+				color++;
+			}
+			if (layer_2 && (1 << scr_x) == (1 << scr_x)) {
+				color+=2;
+			}
 
+		}
+	}
+}
 void PPU::DrawScreen() {
+	ClearScreen();
 	if (texture != 0) {
 		glDeleteTextures(1, &texture);
 	}
 	ImGui::Begin("PPU Screen");
 
-	ImGui::SetWindowSize(ImVec2(350, 350));
-	ImGui::SetWindowPos(ImVec2(580, 75));
+	ImGui::SetWindowSize(ImVec2(256 * 3, 224 * 3+40));
+	ImGui::SetWindowPos(ImVec2(545, 0));
 	ImDrawList* draw_list = ImGui::GetWindowDrawList();
 	int size = 300;
 	ImVec2 base = ImVec2(590, 105);
 	int width = 256;
 	int height = 224;
-	uint8_t* texture_data = new uint8_t[width * height * 3];
 	for (int x = 0; x < width; ++x) {
 		for (int y = 0; y < height; ++y) {
 			int pos = (y * width + x) * 3;
-			texture_data[pos + 0] = (uint8_t)(((float)x / (float)width) * 255);
-			texture_data[pos + 1] = (uint8_t)(((float)y / (float)height) * 255);
-			texture_data[pos + 2] = 0;
+			int color_index = screen_data[pos / 3] * 3;
+			texture_data[pos + 0] = palette[color_index + 0];
+			texture_data[pos + 1] = palette[color_index + 1];
+			texture_data[pos + 2] = palette[color_index + 2];
 		}
 	}
 	unsigned int texture;
@@ -40,18 +70,10 @@ void PPU::DrawScreen() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_data);
 	glGenerateMipmap(GL_TEXTURE_2D);
-	//for (int x = 0; x < width; ++x) {
-	//	for (int y = 0; y < height; ++y) {
-	//		ImColor color = { (float)x / width, (float)y / height, 0.0f, 1.0f };
-
-	//		draw_list->AddRectFilled(ImVec2(base.x + x * (size / width), base.y + y * (size / height)), 
-	//								 ImVec2(base.x + x * (size / width) + size / width, base.y + y * (size / height) + size / height), 
-	//								 color);
-	//	}
-	//}
-	ImGui::Image((ImTextureID)texture, ImVec2(256, 224), ImVec2(0, 1), ImVec2(1, 0));
+	
+	ImGui::Image((ImTextureID)texture, ImVec2(256*3, 224*3), ImVec2(0, 1), ImVec2(1, 0));
 	ImGui::End();
-	delete[] texture_data;
+	//delete[] texture_data;
 
 }
 PPU::PPU(Bus * bus) {
@@ -60,4 +82,8 @@ PPU::PPU(Bus * bus) {
 	}
 	this->bus = bus;
 	scrTex = 0;
+	texture_data = new uint8_t[256 * 224 * 3];
+	screen_data = new uint8_t[256 * 224];
+	texture = 0;
+
 }
--- NESEmulator2024/PPU.h
+++ NESEmulator2024/PPU.h
@@ -11,11 +11,15 @@ public:
 	uint8_t ReadRegister(uint8_t index);
 	void WriteRegister(uint8_t index, uint8_t bits);
 	void DrawScreen();
-	
+	void ClearScreen();
+	void DrawTile(unsigned int index, uint8_t x, uint8_t y, uint8_t colors[4]);
 private:
 	unsigned int texture;
 	Bus* bus;
 	unsigned int scrTex;
 	uint8_t registers[8];
+	uint8_t* screen_data;
+	uint8_t* texture_data;
+
 };
 
